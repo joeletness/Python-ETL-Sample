3b515bf793b0f02c2e0f93a4b1bb0f6442e3f86a
joeletness <joe.letness@gmail.com>
2017-11-05 15:53:50 -0600

    Data can be read and unpacked.
    clean up spike.

--- byte_reader.py
+++ byte_reader.py
@@ -1,10 +1,186 @@
+#!/usr/bin/python
 import os
+from datetime import datetime
+from decimal import Decimal, ROUND_HALF_EVEN, getcontext, ROUND_HALF_DOWN, InvalidOperation
+import struct
+from struct import unpack
+
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
-def get_data(path):
-    file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
-    _file = open(file_path, 'r')
-    data = _file.read()
+class MPS7Object(object):
+    def __init__(self, *args, **kwargs):
+        self.format = None
+        self.version = None
+        self.count = None
+        self.records = []
+        self.file_path = get_file_path(args[0]) if args else None
+
+    def read_records(self):
+        print ''
+        _file = open(self.file_path, 'rb')
+        _bytes = _file.read()
+        for idx, _byte in enumerate(_bytes):
+            if idx >= 9:
+                value = struct.unpack('b', _byte)[0]
+                if value in (0, 1, 2, 3):
+                    record = Record(get_chunks(_bytes, idx, 1, 4, 8, 8), idx)
+                    self.records.append(record)
+        _file.close()
+        return self
+
+    def read_records_alt(self):
+        _file = open(self.file_path, 'rb')
+        _bytes = _file.read()
+
+        header = get_chunks(_bytes, 0, 4, 1, 4)
+        print ''.join(unpack('cccc', header[0]))
+        print unpack('b', header[1])[0]
+        print unpack('>I', header[2])[0]
+
+        idx = 9
+        while True:
+            chunks = get_chunks(_bytes, idx, 1, 4, 8, 8)
+            if not chunks:
+                break
+            record = Record(chunks, idx)
+            idx = next_record_at(idx, record.get_kind())
+            self.records.append(record)
+        _file.close()
+        return self
+
+    @staticmethod
+    def format_list_row(record):
+        print type(record.unpack_amount())
+        template = '{} | {} | {} | {} | {}'
+        result = template.format(
+            str(record.index).rjust(5),
+            record.get_kind().ljust(13),
+            record.get_timestamp(),
+            record.get_user_id(),
+            record.get_amount()
+        )
+        return result
+
+
+def next_record_at(current_position, record_kind):
+    if record_kind in ('Credit', 'Debit'):
+        return current_position + 21
+    return current_position + 13
+
+
+class Record(object):
+    def __init__(self, chunks=None, index=None):
+        self.index = index
+        self._kind = chunks[0] if chunks else None
+        self._timestamp = chunks[1] if chunks else None
+        self._user_id = chunks[2] if chunks else None
+        self._amount = chunks[3] if chunks else None
+
+        if chunks:
+            self.chunks = {
+                'kind': chunks[0],
+                'timestamp': chunks[1],
+                'user_id': chunks[2],
+                'amount': chunks[3],
+            }
+        else:
+            self.chunks = {}
+
+        self.kind = None
+        self.timestamp = None
+        self.user_id = None
+        self.amount = None
+
+    def get_timestamp(self):
+        if self.unpack_timestamp():
+            return datetime.fromtimestamp(self.unpack_timestamp())
+
+    def get_kind(self):
+        _kind = self.unpack_kind()
+        if _kind is None:
+            _kind = self.kind
+        return (
+            'Debit',
+            'Credit',
+            'StartAutopay',
+            'EndAutopay'
+        )[_kind]
+
+    def get_amount(self):
+        if self.amount is not None:
+            return self.amount
+        amount = self.unpack_amount()
+        return float_to_currency(amount) if amount else None
+
+    def get_user_id(self):
+        return self.unpack_user_id()
+
+    def unpack_kind(self):
+        packed = self.chunks.get('kind')
+        return unpack('b', packed)[0] if packed else None
+
+    def unpack_timestamp(self):
+        packed = self.chunks.get('timestamp')
+        return unpack('>I', packed)[0] if packed else None
+
+    def unpack_user_id(self):
+        packed = self.chunks.get('user_id')
+        return unpack('>Q', packed)[0] if packed else None
+
+    def unpack_amount(self):
+        packed = self.chunks.get('amount')
+        return unpack('>d', packed)[0] if packed else None
+
+
+def get_file_path(file_name):
+    return os.path.join(BASE_DIR, file_name)
+
+
+def get_chunks(_bytes, start, *args):
+    result = []
+    for size in args:
+        end = start + size
+        _byte = _bytes[start:end]
+        if not _byte:
+            return []
+        result.append(_byte)
+        start += size
+    return result
+
+
+def float_to_currency(value):
+    return Decimal(Decimal(value).quantize(Decimal('.00'), rounding=ROUND_HALF_EVEN))
+
+
+def parse_data_spike(obj):
+    _file = open(obj.file_path, 'rb')
+    _bytes = _file.read()
+    obj.records = []
+
+    print '--------------DATA READ FROM FILE ----------------'
+    for idx, _byte in enumerate(_bytes):
+        value = struct.unpack('b', _byte)[0]
+        if value in (0, 1, 2, 3):
+            rs = get_chunks(_bytes, idx, 1, 4, 8, 8)
+            record = Record(rs)
+            record.kind = unpack('b', rs[0])[0]
+            record.timestamp = datetime.fromtimestamp(unpack('>I', rs[1])[0])
+            record.user_id = unpack('>Q', rs[2])[0]
+            record.amount = round(unpack('>d', rs[3])[0], 8)
+
+            _record_array = [
+                unpack('b', rs[0])[0],
+                datetime.fromtimestamp(unpack('>I', rs[1])[0]),
+                unpack('>Q', rs[2])[0]
+            ]
+            if value in (0, 1):
+                amount = round(unpack('>d', rs[3])[0], 2)
+                _record_array.append(amount)
+
+            # print str(idx).rjust(5), ' ', record.get_kind().ljust(13), record.timestamp, ' ', str(record.user_id).ljust(20), record.get_amount()
+            obj.records.append(record)
+
     _file.close()
-    return data
+
+    return obj
--- test_byte_reader.py
+++ test_byte_reader.py
@@ -1,8 +1,140 @@
 from unittest import TestCase
-from byte_reader import get_data
+import time
+from datetime import datetime
+from struct import pack
+from decimal import Decimal
+from byte_reader import MPS7Object, Record, parse_data_spike, get_chunks, float_to_currency
+from byte_reader import next_record_at
 
 
-class TestByteReader(TestCase):
-    def test_get_data_reads_a_given_file(self):
-        data = get_data('sample.dat')
-        self.assertEqual('sampleString',data)
+def test_get_chunks__when_passed_blob_with_start_and_chunk_size__returns_chunks():
+    blob = 'pearcowtomatotunabeeorange'
+    start_index = 0
+    result = get_chunks(blob, start_index, 4, 3, 6)
+    assert result == ['pear', 'cow', 'tomato']
+
+    start_index = 13
+    result = get_chunks(blob, start_index, 4, 3, 6)
+    assert result == ['tuna', 'bee', 'orange']
+
+
+def test_get_chunks__when_start_index_out_of_range__returns_none():
+    blob = 'pearcowtomatotunabeeorange'
+    start_index = 45
+    result = get_chunks(blob, start_index, 4, 3, 6)
+    assert result == []
+
+
+def test_parse_data_spike():
+    obj = MPS7Object('data.dat')
+    parse_data_spike(obj)
+    assert len(obj.records) == 92
+    assert obj.records[0].chunks
+    assert 'Credit' in obj.records[0].get_kind()
+    print
+
+
+def test_float_to_currency__converts_double_to_decimal_with_two_places():
+    assert str(float_to_currency(384.61670768)) == '384.62'
+    assert float_to_currency(384.61670768) == Decimal('384.62')
+    foo = float_to_currency(384.61670768)
+
+
+def test_next_offest__when_debit_or_credit__return_location_of_next_record_21_bytes_ahead():
+    assert next_record_at(30, 'Credit') == 51
+    assert next_record_at(30, 'Debit') == 51
+
+
+def test_next_offest__when_not_debit_or_credit__return_location_of_next_record_13_bytes_ahead():
+    assert next_record_at(30, 'Not Credit') == 43
+
+
+class TestMPS7Object(TestCase):
+    def test_read_records(self):
+        obj = MPS7Object('data.dat')
+        obj.read_records()
+        assert len(obj.records) == 88
+
+    def test_read_records_alt(self):
+        obj = MPS7Object('data.dat')
+        obj.read_records_alt()
+        assert len(obj.records) == 72
+
+    # def test_set_header(self):
+    #     obj = MPS7Object('data.dat')
+    #     obj.read_records_alt()
+    #     assert obj.count == 71
+
+    def test_format_list_row(self):
+
+        # 9   Debit         2014-02-22 16:42:25   4136353673894269217  604.27433556
+
+        print_he = ' byte | kind          | timestamp           | user_id             | amt'
+        expected = '    9 | Debit         | 2014-02-22 16:42:25 | 4136353673894269217 | 604.27'
+        obj = MPS7Object('data.dat')
+        obj.read_records()
+
+        result = obj.format_list_row(obj.records[0])
+        assert result == expected
+
+
+class TestRecord(TestCase):
+    def test_new_instance__when_positional_chunks_are_given__sets_dictionary(self):
+        chunks = ['kind-byte', 'timestamp-bytes', 'user_id-bytes','amount-bytes']
+        record = Record(chunks)
+        assert record.chunks['kind'] == 'kind-byte'
+        assert record._timestamp == 'timestamp-bytes'
+        assert record._user_id == 'user_id-bytes'
+        assert record._amount == 'amount-bytes'
+
+    def test_unpack_kind__returns_unpacked_value(self):
+        record = Record()
+        record.chunks['kind'] = pack('b', 2)
+        assert record.unpack_kind() == 2
+
+    def test_unpack_timestamp__returns_unpacked_value(self):
+        fake_timestamp_int = int(time.mktime(datetime.now().timetuple()))
+        record = Record()
+        record.chunks['timestamp'] = pack('>I', fake_timestamp_int)
+        assert record.unpack_timestamp() == fake_timestamp_int
+
+    def test_unpack_user_id__returns_unpacked_value(self):
+        record = Record()
+        record.chunks['user_id'] = pack('>Q', 2456938384156277127)
+        assert record.unpack_user_id() == 2456938384156277127
+
+    def test_unpack_amount__returns_unpacked_value(self):
+        record = Record()
+        record.chunks['amount'] = pack('>d', 42.473264)
+        assert record.unpack_amount() == 42.473264
+
+    def test_get_kind__returns_readable_value(self):
+        record = Record()
+        record.chunks['kind'] = pack('b', 0)
+        assert 'Debit' in record.get_kind()
+
+    def test_get_timestamp__when_no_data__returns_none(self):
+        record = Record()
+        assert record.get_timestamp() is None
+
+    def test_get_timestamp__when_data__returns_expected_timestamp(self):
+        expected = datetime(2017, 11, 5)
+        fake_timestamp_int = int(time.mktime(expected.timetuple()))
+        record = Record()
+        record.chunks['timestamp'] = pack('>I', fake_timestamp_int)
+        assert record.get_timestamp() == expected
+
+    def test_get_user_id__when_no_data__return_none(self):
+        record = Record()
+        record.chunks['user_id'] = pack('>Q', 2456938384156277127)
+        assert record.get_user_id() == 2456938384156277127
+
+    def test_get_amount__when_no_data__return_none(self):
+        record = Record()
+        assert record.get_amount() is None
+
+    def test_get_amount__when_data__return_decimal_value(self):
+        record = Record()
+        record.chunks['kind'] = pack('b', 1)
+        record.chunks['amount'] = pack('>d', 42.4)
+        assert record.get_amount() == Decimal('42.40')
